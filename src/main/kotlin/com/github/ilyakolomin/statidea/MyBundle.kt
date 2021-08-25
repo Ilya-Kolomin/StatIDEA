5bc9387e1f9c2b0cd8dfaf0bf8ad51f2df7efe0b
GitHub Action <action@github.com>
2021-08-25 09:01:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/ilyakolomin/statidea/MyBundle.kt
+++ src/main/kotlin/com/github/ilyakolomin/statidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ilyakolomin.statidea
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ilyakolomin/statidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ilyakolomin/statidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ilyakolomin.statidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ilyakolomin.statidea.MyBundle
 
 class MyApplicationService {
 
